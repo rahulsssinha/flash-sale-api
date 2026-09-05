394663667df10f535b6e214923fb9e78d43519cb
Rahul Sinha <sinharahul591@gmail.com>
2026-09-05 19:13:01 +0530

    Initial api limiter

---
+++ src/main/java/com/example/flashsale/FlashSaleApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.flashsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashSaleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashSaleApiApplication.class, args);
+	}
+
+}
